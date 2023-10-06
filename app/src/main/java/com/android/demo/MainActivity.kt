420c8c66a82254ed2772c6065467ad20a1b877ab
vikas <vk98659@gmail.com>
2023-10-06 19:32:11 +0530

    new Commit

--- app/src/main/java/com/android/demo/MainActivity.kt
+++ app/src/main/java/com/android/demo/MainActivity.kt
@@ -3,17 +3,29 @@ package com.android.demo
 
 import android.content.Intent
 import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.MediaController
 import androidx.core.net.toUri
 import com.android.demo.databinding.ActivityMainBinding
+import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.util.FitPolicy
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 
 private const val TAG = "MainAct"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    // url of our PDF file.
+    var pdfUrl = "https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -43,8 +55,36 @@ class MainActivity : AppCompatActivity() {
 
     private fun onButtonClickEvent() {
         Log.i(TAG, "Button Clicked")
-        val url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(intent)
+        val url = "android.resource://$packageName/raw/${R.raw.sample}"
+        //val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        val uri = url.toUri().buildUpon().scheme("https").build()
+        Log.i(TAG, "URI IS: $uri")
+        RetrievePDFFromURL(binding.pdfView).execute(pdfUrl)
+    }
+    inner class RetrievePDFFromURL(private val pdfView: PDFView) :
+        AsyncTask<String, Void, InputStream>() {
+
+        override fun doInBackground(vararg params: String?): InputStream? {
+            var inputStream: InputStream? = null
+            try {
+                // on below line we are creating an url
+                // for our url which we are passing as a string.
+                val url = URL(params.get(0))
+                val urlConnection: HttpURLConnection = url.openConnection() as HttpsURLConnection
+                if (urlConnection.responseCode == 200) {
+                    inputStream = BufferedInputStream(urlConnection.inputStream)
+                }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                return null;
+            }
+            return inputStream;
+        }
+
+        override fun onPostExecute(result: InputStream?) {
+            pdfView.fromStream(result).load()
+
+        }
     }
 }
